37ab85c924571a8ac271cf551b21843072ca2d58
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-06-30 01:22:27 +0600

    Add solved task01-array.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,16 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	sum := float32(0)
+	lastNonZeroIndex := 0
+	for i := 0; i < len(input); i++ {
+		if input[i] != 0 {
+			lastNonZeroIndex = i
+		}
+	}
+	for i := 0; i <= lastNonZeroIndex; i++ {
+		sum += input[i]
+	}
+	result = sum / float32(lastNonZeroIndex+1)
+	return result
 }
