c0cb4b09615876c97b92d7b40c1b54027c853018
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-07-02 19:28:57 +0600

    Add inplace reverse method

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,14 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	left := 0
+	right := len(input) - 1
+	for left < right {
+		temp := input[left]
+		input[left] = input[right]
+		input[right] = temp
+		left += 1
+		right -= 1
+	}
+	return input
 }
